2441253bf79bbb5c15619962b9834bf8fb45ef08
Horacio Fernandes <horaciofernandes@MacBook-Pro-de-Horacio.local>
2021-01-26 09:43:57 -0300

    Handle API errors globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,59 @@
-import "@/globals.css";
+import { useRouter } from "next/router";
+import { ErrorBoundary } from "react-error-boundary";
+import { SWRConfig } from "swr";
+import { ToastContainer, toast } from "react-toastify";
 import Layout from "@/common/layout/Layout";
+import "@/globals.css";
+// eslint-disable-next-line @theorem/no-imports-down
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <ErrorBoundary onError={<ErrorNotification router={router} />}>
+        <SWRConfig
+          value={{
+            onError: (error, key) => {
+              if (error.status !== 403 && error.status !== 404) {
+                toast.error(<ErrorNotification router={router} />);
+              }
+            },
+          }}
+        >
+          <Layout>
+            <Component {...pageProps} />;
+          </Layout>
+        </SWRConfig>
+      </ErrorBoundary>
+      <ToastContainer
+        position="top-center"
+        autoClose={false}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover
+        limit={1}
+      />
+    </>
+  );
+}
+
+function ErrorNotification({ router }) {
+  return (
+    <div role="alert">
+      Something went wrong!
+      <p>
+        Please{" "}
+        <a className="font-bold underline" onClick={() => router.reload()}>
+          try again
+        </a>
+      </p>
+    </div>
   );
 }
 
--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -1,16 +1,21 @@
 import useSWR from "swr";
-import fetch from "unfetch";
+import fetch from "isomorphic-fetch";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const fetcher = (path) =>
-  fetch(`${BASE_URL}/${path}`).then((r) => r.json());
-
-export function useApiFetch({ path }) {
-  const { error, ...rest } = useSWR(path, fetcher);
-  if (error) {
-    console.error(error);
+export const fetcher = async (path) => {
+  const res = await fetch(`${BASE_URL}/${path}`);
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.status = res.status;
+    throw error;
   }
+  return res.json();
+};
 
-  return rest;
+export function useApiFetch({ path, initialData }) {
+  return useSWR(path, fetcher, { initialData });
 }
